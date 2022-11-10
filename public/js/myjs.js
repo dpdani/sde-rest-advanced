becab09da55f3fd1803d4d35fa6a9071348b6fac
Stefano Faccio <stefano.faccio@studenti.unitn.it>
2022-11-10 14:53:32 +0100

    Added Style to index page

---
+++ public/js/myjs.js
@@ -0,0 +1,8 @@
+function myonloadfunction()
+{
+    let lat = document.getElementById("lat");
+    let lon = document.getElementById("lon");
+
+    lat.value = 41.40338;
+    lon.value = 2.17403;
+}
