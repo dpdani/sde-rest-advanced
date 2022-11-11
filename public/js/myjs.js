351fb8ad7fb562cf9ea6aebe937739fbde5da0a4
Stefano Faccio <stefano.faccio@studenti.unitn.it>
2022-11-11 13:16:27 +0100

    Added Address input filed, utility buttons

--- public/js/myjs.js
+++ public/js/myjs.js
@@ -1,8 +1,10 @@
-function myonloadfunction()
+function loadCordinates(lalValue = 41.40338, lonValue = 2.17403, addValue = "Sagrada Familia")
 {
     let lat = document.getElementById("lat");
     let lon = document.getElementById("lon");
+    let add = document.getElementById("add");
 
-    lat.value = 41.40338;
-    lon.value = 2.17403;
+    lat.value = lalValue;
+    lon.value = lonValue;
+    //add.value = addValue;
 }
