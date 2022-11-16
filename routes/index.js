05526a65d39fb081f8360d75ae276a796cbabbb4
giovannirigotti <rigotti.giovanni@gmail.com>
2022-11-16 18:15:31 +0100

    added searchOSM method to move the map given a place name

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch  = require('node-fetch');
 const router = express.Router();
 const lab4client = require('../lab4client/lab4client')
 
@@ -11,6 +12,22 @@ router.get("/test", async function (req, res, next) {
   const eventIds = await lab4client.searchEvents(45.45, 11.02, "", new Date("2022-10-17T18:00:00Z"));
   let events = await lab4client.prepareEventsForMap(eventIds);
   await res.send(events);
+});
+
+
+router.get('/searchOSM', (req, res, next) => {
+  let name = req.query.name;
+  fetch('https://nominatim.openstreetmap.org/search?' + new URLSearchParams({
+	  q:name,
+	  format:'json'
+  })).then(async(response)=>{
+	  let body = await response.text()
+	  let json = JSON.parse(body)
+    console.log(req.path);
+    req.path = "/";
+    console.log(req.path);
+	  res.redirect('../?' + new URLSearchParams({lat:json[0].lat, lon:json[0].lon}))
+  });
 });
 
 module.exports = router;
