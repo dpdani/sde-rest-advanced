2f3f7632c82439dcc059faa658f4f157326a6fa9
Camilla9347 <50362663+Camilla9347@users.noreply.github.com>
2022-11-16 13:09:21 +0100

    marker added on map

--- webmap.js
+++ webmap.js
@@ -10,3 +10,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var marker = L.marker([lat, lon]).addTo(map);
