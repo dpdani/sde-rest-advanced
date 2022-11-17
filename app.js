75ebcc603c1f85557d0e92270c6ba4c70cf289ea
Camilla9347 <50362663+Camilla9347@users.noreply.github.com>
2022-11-17 17:29:50 +0100

    Unesuful files deleted and code cleaned + post structure added (work in progress)

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const latLonCoordinatesRouter = require('./routes/lat-lon-coordinates');
-
 const app = express();
 
 app.use(logger('dev'));
@@ -16,7 +12,4 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/lat-lon-coordinates', latLonCoordinatesRouter);
-
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fetch  = require('node-fetch');
 const router = express.Router();
 const lab4client = require('../lab4client/lab4client')
+const functions = require('../routes/functions')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +13,41 @@ router.get("/test", async function (req, res, next) {
   const eventIds = await lab4client.searchEvents(45.454967071564106, 11.02984920555249, "", new Date("2022-10-18"));
   let events = await lab4client.prepareEventsForMap(eventIds);
   await res.send(events);
+});
+
+/* POST lat lon coordinate listing. */
+router.post('/', function(request, res, next) {
+  if (isLatitude(request.body.lat) && isLongitude(request.body.lon)){
+    const lat = request.body.lat;
+    const lon = request.body.lon;
+
+    const options = {
+      headers: {'User-Agent': 'some app v1.3 (example@gmail.com)'},
+      host: 'nominatim.openstreetmap.org',
+      method: 'GET',
+      path: `/reverse?lat=${lat}&lon=${lon}&format=json`
+    };
+
+    const req = https.request(options, function (res) {
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+      });
+    });
+
+    req.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+    });
+    
+    // write data to request body
+    req.write('data\n');
+    req.write('data\n');
+    req.end();
+  } else {
+    //refresh previous html page on main URL
+  }
 });
 
 
